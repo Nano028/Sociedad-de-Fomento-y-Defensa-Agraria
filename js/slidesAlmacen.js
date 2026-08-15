f920354c30f341e586278cef96972e6801736ab9
Martin Gonzalez <mfgc67@gmail.com>
2026-08-15 09:52:29 -0300

    actualizacion agosto

--- js/slidesAlmacen.js
+++ js/slidesAlmacen.js
@@ -20,3 +20,61 @@ document.querySelectorAll(".gallery-slider").forEach((slider) => {
     update();
   });
 });
+
+const heroSlider = document.querySelector(".hero-slider");
+
+if (heroSlider) {
+  const heroSlides = heroSlider.querySelectorAll(".hero-slide");
+  const heroDots = heroSlider.querySelectorAll(".hero-dot");
+  const previousButton = heroSlider.querySelector(".hero-prev");
+  const nextButton = heroSlider.querySelector(".hero-next");
+
+  let currentHeroSlide = 0;
+  let heroInterval;
+
+  function showHeroSlide(index) {
+    heroSlides[currentHeroSlide].classList.remove("active");
+    heroDots[currentHeroSlide].classList.remove("active");
+
+    currentHeroSlide = (index + heroSlides.length) % heroSlides.length;
+
+    heroSlides[currentHeroSlide].classList.add("active");
+    heroDots[currentHeroSlide].classList.add("active");
+  }
+
+  function startHeroSlider() {
+    heroInterval = setInterval(() => {
+      showHeroSlide(currentHeroSlide + 1);
+    }, 4500);
+  }
+
+  function restartHeroSlider() {
+    clearInterval(heroInterval);
+    startHeroSlider();
+  }
+
+  previousButton.addEventListener("click", () => {
+    showHeroSlide(currentHeroSlide - 1);
+    restartHeroSlider();
+  });
+
+  nextButton.addEventListener("click", () => {
+    showHeroSlide(currentHeroSlide + 1);
+    restartHeroSlider();
+  });
+
+  heroDots.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+      showHeroSlide(index);
+      restartHeroSlider();
+    });
+  });
+
+  heroSlider.addEventListener("mouseenter", () => {
+    clearInterval(heroInterval);
+  });
+
+  heroSlider.addEventListener("mouseleave", startHeroSlider);
+
+  startHeroSlider();
+}
